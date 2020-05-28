460f3032ae6a8c7c491e1c23bca4ab2e289fa165
Julieta Sdrubolini <julisdru@gmail.com>
2020-05-28 01:43:00 -0300

    actualizacion de icono y splash, creacion de splash dinamico y comienzo de login

--- lacomanda/src/app/app.component.ts
+++ lacomanda/src/app/app.component.ts
@@ -33,7 +33,7 @@ export class AppComponent {
 
     setTimeout(() => {       //// Esta propiedad es para que deje de ejecutarse el splash, se le indica la cantidad de tiempo que se quiere ejecutar el splash
       this.splash = false;
-    }, 7200);  
+    }, /*7200*/);  
 // 7200
   }
 }
