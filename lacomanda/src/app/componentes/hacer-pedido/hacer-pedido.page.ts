11da2d23bfe1be0ba53600daf913310e23439fcc
nsama95 <natalia.samaniego.95@gmail.com>
2020-06-21 14:53:24 -0300

    pedido en proceso

--- lacomanda/src/app/componentes/hacer-pedido/hacer-pedido.page.ts
+++ lacomanda/src/app/componentes/hacer-pedido/hacer-pedido.page.ts
@@ -3,8 +3,14 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { ValueAccessor } from '@ionic/angular/directives/control-value-accessors/value-accessor';
+import { DatabaseService } from 'src/app/servicios/database.service';
 
-
+interface Producto {
+  tipo:string,
+  nombre:string,
+ descripcion:string,
+  precio : number,
+}
 @Component({
   selector: 'app-hacer-pedido',
   templateUrl: './hacer-pedido.page.html',
@@ -12,14 +18,30 @@ import { ValueAccessor } from '@ionic/angular/directives/control-value-accessors
 })
 
 
+
 export class HacerPedidoPage implements OnInit {
+ 
   lista = [];
+  listajson : Producto[];
   bandera="";
   @Input() tipo;
+
+  cantidad = 0;
+  totalProducto= 0;
+  productoJson:Producto;
+ /*productoJson = {
+      tipo:"",
+      nombre:"",
+     descripcion: "",
+      precio : "",
+      //foto: ["","",""],
+    }*/
+    productoA: string;
   constructor(
     private firestore : AngularFirestore,
     public alertController: AlertController ,
     public router : Router,
+    private bd : DatabaseService,
   ) { }
 
   ngOnInit() {
@@ -63,14 +85,19 @@ export class HacerPedidoPage implements OnInit {
           
           this.lista = [];
       
-          datos.forEach( (dato:any) =>{
+         datos.forEach( (dato: any) =>{
+
+            
       
+    //let producto= this.productoJson;
+    //this.listajson.push(this.productoJson);
          this.lista.push(dato);  
             // <--- LISTA DE USUARIOS.
-      //console.log(this.lista);
+      
           });
       
         })
+       // console.log(this.listajson);
       }
         //this.tipo = datos.data().tipo;
        // 
@@ -84,6 +111,44 @@ export class HacerPedidoPage implements OnInit {
       }*/
     
 }
+
+/*tomados(){
+ 
+  
+
+    this.firestore.collection('productos').get().subscribe((querySnapShot) => {
+      querySnapShot.forEach( (dato) => {
+       
+         if(dato.data().tipo =='comida'){
+
+          this.productoJson = {
+            nombre:dato.data().nombre,
+           tipo:dato.data().tipo,
+           precio:dato.data().precio,
+          
+         descripcion:dato.data().descripcion
+          }
+            
+
+            //console.log(this.productoJson);
+           
+           this.listajson.push(this.productoJson);
+
+        }
+         
+        
+
+          // esto pone la lista vacía para que quede facherisima.
+          
+
+      })
+    
+      
+      console.log(this.listajson);
+    })
+   
+    
+}*/
 
   async alert(){
 
@@ -133,5 +198,68 @@ export class HacerPedidoPage implements OnInit {
   
 }
 
+calcularProducto(nombre:string,signo: string){
+
+  this.firestore.collection('productos').get().subscribe((querySnapShot) => {
+    
+    querySnapShot.forEach((datos => {
+
+if(signo === '+' && datos.data().nombre === nombre && datos.data().cantidad === 0 )
+{
+  let auxCantidad= datos.data().cantidad;
+   auxCantidad=this.cantidad++;
+
+
+
+  console.log(datos.data().nombre);
+  console.log(this.cantidad);
+  this.bd.actualizar('productos',auxCantidad,datos.id);
+  console.log(datos.data().cantidad);
+  
+
+
+ 
+} 
+else if(signo === '-' && datos.data().id === nombre)
+{
+ 
+  if(this.cantidad>0)
+  {
+    this.cantidad--;
+  }
+  else
+  {
+this.cantidad= 0;
+/**un toas para que el usuario entienda que no puede seguir tocando ah */
+  }
+
+}
+/*datos.data().cantidad=this.cantidad;
+let cantidad = this.cantidad;
+this.bd.actualizar('productos',cantidad,datos.data().cantidad);*/
+
+}));
+})
+  /*this.firestore.collection('productos').get().subscribe((querySnapShot) => {
+    
+  querySnapShot.forEach(datos => {
+
+   // console.log(nombre);
+    if(datos.data().nombre == nombre){
+     // console.log(datos.data().nombre);
+      let precioProducto= datos.data().precio;
+      console.log(precioProducto);
+      console.log(this.cantidad);
+    
+     this.totalProducto = precioProducto * this.cantidad;
+      console.log(this.totalProducto);
+     
+    }
+  });
+})
+*/
+
+
+}
 
 }
